1ff85c2362a47678ac7c70a661648c4cc8304563
theacehorsemen <65445074+theacehorsemen@users.noreply.github.com>
2020-05-17 20:17:25 +0530

    Delete chatbot.js

--- chatbot.js
+++
@@ -1,6 +0,0 @@
-<script SameSite="None; Secure" src="https://static.landbot.io/landbot-widget/landbot-widget-1.0.0.js"></script>
-<script>
-  var myLandbot = new LandbotLivechat({
-    index: 'https://landbot.io/u/H-479135-8AMZUXNZB161SDDF/index.html',
-  });
-</script>
