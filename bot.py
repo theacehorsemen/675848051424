2b9010a1769e0190104b42380f9985a7596c4669
theacehorsemen <65445074+theacehorsemen@users.noreply.github.com>
2020-05-18 14:43:03 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,13 @@
+import random
+import string
+
+print('CONGRATS FOR REACHING THIS STAGE')
+A = input('ENTER THE NO. YOU FOUND IN THE PREVIOUS IMAGE' ":" " ")
+N = 25
+res = ''.join(random.choices(string.ascii_uppercase +
+                             string.digits, k = N))
+b = str(res)
+if A == ("277"):
+    print('OKAY GOOD. TAKE THIS TOKEN AND  :', b)
+else:
+    print('ughh ohh! wrong code')
